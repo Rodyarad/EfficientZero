84828cdac9fdb240643ceee65ef4b7b59a5639ab
Rodyarad <r.vakhitov2018@gmail.com>
2025-06-26 13:25:11 +0300

    added shapes2d

--- main.py
+++ main.py
@@ -73,6 +73,8 @@ if __name__ == '__main__':
     # import corresponding configuration , neural networks and envs
     if args.case == 'atari':
         from config.atari import game_config
+    elif args.case == 'shapes2d':
+        from config.shapes2d import game_config
     else:
         raise Exception('Invalid --case option')
 

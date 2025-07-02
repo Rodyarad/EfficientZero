0c065141ce5f66f4f442b3f0224cb915056e4a54
Rodyarad <r.vakhitov2018@gmail.com>
2025-07-02 09:27:01 +0300

    shapes2d is added

---
+++ test_shapes2d.sh
@@ -0,0 +1,14 @@
+set -ex
+export CUDA_DEVICE_ORDER='PCI_BUS_ID'
+export CUDA_VISIBLE_DEVICES=0
+export WANDB_ENTITY="r-rodionvahitoff"
+
+python main.py --env Navigation5x5-v0 --case shapes2d_test --opr train --force \
+  --num_gpus 1 --num_cpus 4 --cpu_actor 1 --gpu_actor 1 \
+  --seed 0 \
+  --p_mcts_num 4 \
+  --use_priority \
+  --use_max_priority \
+  --amp_type 'torch_amp' \
+  --info 'EfficientZero-V1' \
+  --object_store_memory 2147483648

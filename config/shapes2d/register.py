53d0a03727fd750e59e28b8916ea29797994bee4
Rodyarad <r.vakhitov2018@gmail.com>
2025-08-25 17:36:06 +0300

    change observation in shapes2d

--- config/shapes2d/register.py
+++ config/shapes2d/register.py
@@ -22,6 +22,27 @@ register(
 )
 
 register(
+    'Navigation5x5-coord-v0',
+    entry_point='config.shapes2d.shapes2d:Shapes2d',
+    max_episode_steps=100,
+    kwargs={
+        'observation_type': 'coordinates',
+        'return_state': False,
+        'border_walls': True,
+        'n_boxes': 5,
+        'n_goals': 1,
+        'n_static_boxes': 0,
+        'static_goals': True,
+        'width': 5,
+        'render_scale': 10,
+        'channel_wise': False,
+        'ternary_interactions': False,
+        'embodied_agent': False,
+        'do_reward_push_only': False,
+    },
+)
+
+register(
     'Navigation5x5Interaction-v0',
     entry_point='config.shapes2d.shapes2d:Shapes2d',
     max_episode_steps=100,
